eab0c74c06773733a51e6e2a91f2c43d89a0d25f
jose <jose.rodriguez@prominentcode.com>
2022-06-08 00:35:05 -0700

    apollo client; header; add/delete clients

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import Header from './components/Header';
+import Clients from './components/Clients';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+// removes error message from console
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          }
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          }
+        }
+      }
+    }
+  }
+});
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache
+})
+
+function App() {
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <Clients />
+        </div>
+      </ApolloProvider>
+    </>
+  );
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const colors = require('colors')
 require('dotenv').config()
 const { graphqlHTTP } = require('express-graphql')
@@ -10,6 +11,8 @@ const app = express()
 
 // connect to mongoDB
 connectDB();
+
+app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
     schema,
