e2cc10223df92bbe77f68909874e01d4f0a6d052
jose <jose.rodriguez@prominentcode.com>
2022-06-08 17:08:34 -0700

    Build project queries and view

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import Projects from "./components/Projects";
 import Clients from "./components/Clients";
 import AddClientModal from "./components/AddClientModal";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
@@ -35,6 +36,7 @@ function App() {
         <Header />
         <div className="container">
           <AddClientModal />
+          <Projects />
           <Clients />
         </div>
       </ApolloProvider>
---
+++ client/src/queries/projectQueries.js
@@ -0,0 +1,12 @@
+import { gql } from "@apollo/client";
+
+const GET_PROJECTS = gql`
+  query GetProjects {
+    projects {
+        id
+        name
+        status
+    }
+  }`
+
+  export { GET_PROJECTS };
